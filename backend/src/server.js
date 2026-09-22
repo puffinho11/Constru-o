a6d09640bd087701625d0055b2b7f547595d5bdb
Luiz Otávio Puff <142064806+puffinho11@users.noreply.github.com>
2026-09-22 14:03:29 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -59,7 +59,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://constru-o-pi.vercel.app",
+  "https://construopi.vercel.app/",
   process.env.FRONT_URL,
 ].filter(Boolean);
 
